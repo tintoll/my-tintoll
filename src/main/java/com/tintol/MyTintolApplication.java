6ffa91a7307d4dd4d986214467d752bdc36c59d1
tintoll <belselios@gmail.com>
2017-01-13 01:44:11 +0900

    init commit

---
+++ src/main/java/com/tintol/MyTintolApplication.java
@@ -0,0 +1,12 @@
+package com.tintol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTintolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTintolApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tintol/MyTintolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tintol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTintolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
